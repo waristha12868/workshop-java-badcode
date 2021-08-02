a3ed480036a56f2f53b665648951db545b681ce6
waristha12868 <54305064+waristha12868@users.noreply.github.com>
2021-08-02 16:05:56 +0700

    add case06

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -5,22 +5,82 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RegisterBusinessTest {
+public class RegisterBusinessTest {
+
     @Test
-    @DisplayName("First name of Speaker มีค่าเป็น null ซึ่งทำให้เกิด exception ขึ้นมา")
+    @DisplayName("First name ของ speaker มีค่าเป็น null")
     public void case01() {
         RegisterBusiness registerBusiness = new RegisterBusiness();
         Exception exception = assertThrows(ArgumentNullException.class, () ->
-                registerBusiness.register(null, new Speaker()));
+                registerBusiness.register(null, new Speaker())
+        );
         assertEquals("First name is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Last name ของ speaker มีค่าเป็น null")
+    public void case02() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FName");
+        Exception exception = assertThrows(ArgumentNullException.class, () ->
+                registerBusiness.register(null, speaker)
+        );
+        assertEquals("Last name is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Email ของ speaker มีค่าเป็น null")
+    public void case03() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FName");
+        speaker.setLastName("LName");
+        Exception exception = assertThrows(ArgumentNullException.class, () ->
+                registerBusiness.register(null, speaker)
+        );
+        assertEquals("Email is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Domain ของ email ไม่ถูกต้อง")
+    public void case04() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FName");
+        speaker.setLastName("LName");
+        speaker.setEmail("FName.gmail.com");
+        Exception exception = assertThrows(DomainEmailInvalidException.class, () ->
+                registerBusiness.register(null, speaker)
+        );
+        assertNull(exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Domain ของ email ไม่ support")
+    public void case05() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FName");
+        speaker.setLastName("LName");
+        speaker.setEmail("FName@fail.com");
+        Exception exception = assertThrows(SpeakerDoesntMeetRequirementsException.class, () ->
+                registerBusiness.register(null, speaker)
+        );
+        assertEquals("Speaker doesn't meet our standard rules.", exception.getMessage());
     }
     
     @Test
-    @DisplayName("Last name of Speaker มีค่าเป็น null ซึ่งทำให้เกิด exception ขึ้นมา")
-    public void case02() {
+    @DisplayName("ไม่สามารถบันทึก speaker ลง database ได้")
+    public void case06() {
         RegisterBusiness registerBusiness = new RegisterBusiness();
-        Exception exception = assertThrows(ArgumentNullException.class, () ->
-                registerBusiness.register(null, speaker));
-        assertEquals("Last name is required.", exception.getMessage());
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("FName");
+        speaker.setLastName("LName");
+        speaker.setEmail("FName@gmail.com");
+        Exception exception = assertThrows(SaveSpeakerException.class, () ->
+                registerBusiness.register(null, speaker)
+        );
+        assertEquals("Can't save a speaker.", exception.getMessage());
     }
 }
